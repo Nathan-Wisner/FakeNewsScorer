91edec8bc50db085bcba962eeb491eb1eea1d10d
nwisner <nwisner@eecs.wsu.edu>
2020-02-22 15:12:45 -0800

    Added more intricate scoring system, is in beta

--- Hoaxy+Score+Retrieval.py
+++ Hoaxy+Score+Retrieval.py
@@ -38,7 +38,7 @@ data = res.read()
 print(data.decode("utf-8"))
 
 
-# In[47]:
+# In[144]:
 
 import http.client
 import json
@@ -50,16 +50,79 @@ headers = {
     'x-rapidapi-key': "8bd7508073mshe5c51e555173ee1p1a803ajsnebd7566cb65b"
     }
 
-conn.request("GET", "/articles?sort_by=relevant&use_lucene_syntax=true&query=Bernie+Sanders+Is+A+Nazi", headers=headers)
+def categorizeScore(score, median):
+    if(score < median - (median) * .05):
+        return "Likely True"
+    if (score > median - (median * .1) and score < median + (median * .05)):
+        return "Possibly True"
+    if (score > median + (median) and score < median + (median * .05)):
+        return "Possibly False"
+    if (score > median + (median * .05) and score < median + (median * .1)):
+        return "Likely False"
+    else:
+        return "Very Likely False"
 
-res = conn.getresponse()
-data = res.read()
-JSON = json.loads(data.decode("utf-8"))
+def getMean(JSON):
+    counter = 0
+    totalScore = 0
+    for item in JSON["articles"]:
+        if(item["site_type"] == "claim"):
+            counter = counter  +1
+            totalScore = totalScore + int(item["score"])
+    return totalScore / counter
 
-for item in JSON["articles"]:
-    print(item["title"])
-    print(item["site_type"])
-    print(item["score"])
+def printScores(JSON):
+    mean = getMean(JSON)
+    print(mean)
+    for item in JSON["articles"]:
+        print(item["title"])
+        print(categorizeScore(item["score"], mean))
+        #print(item["score"])
+
+def getHeadlineScore(title):
+
+    title = title.replace(" ", "+")
+    title = title.replace("_", "+")
+    title = title.replace("-", "+")
+
+    conn.request("GET", "/articles?sort_by=relevant&use_lucene_syntax=false&query=" + title, headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    JSON = json.loads(data.decode("utf-8"))
+    getMean(JSON)
+    printScores(JSON)
+getHeadlineScore("Obama")
+
+
+# In[94]:
+
+import requests
+
+url = "https://api-hoaxy.p.rapidapi.com/latest-articles"
+
+querystring = {"domains":'[infowars.com]',"past_hours":"100"}
+
+headers = {
+    'x-rapidapi-host': "api-hoaxy.p.rapidapi.com",
+    'x-rapidapi-key': "8bd7508073mshe5c51e555173ee1p1a803ajsnebd7566cb65b"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+
+# In[96]:
+
+get_ipython().system('pip install torch===1.4.0 torchvision===0.5.0 -f https://download.pytorch.org/whl/torch_stable.html')
+    
+import torch
+
+
+# In[98]:
+
+import torch, torchtext
 
 
 # In[ ]:
