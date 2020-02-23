0fc2c58be3a358a928db336c791c0da0a26a8998
nwisner <nwisner@eecs.wsu.edu>
2020-02-23 08:27:31 -0800

    Created expanded URL sequencing, updated scoring and added bot files

--- PythonScripts/Hoaxy.py
+++ PythonScripts/Hoaxy.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[ ]:
@@ -12,21 +11,21 @@ import torchtext
 from urllib.parse import urlparse
 from URL import getHeadline
 
-
 conn = http.client.HTTPSConnection("api-hoaxy.p.rapidapi.com")
 
 verifyHeaders = {
     'x-rapidapi-host': "adverifai-api.p.rapidapi.com",
     'x-rapidapi-key': "8bd7508073mshe5c51e555173ee1p1a803ajsnebd7566cb65b"
-    }
+}
 
 hoaxiHeaders = {
     'x-rapidapi-host': "api-hoaxy.p.rapidapi.com",
     'x-rapidapi-key': "8bd7508073mshe5c51e555173ee1p1a803ajsnebd7566cb65b"
-    }
+}
+
 
 def categorizeScore(score, median):
-    if(score < median - (median) * .03):
+    if (score < median - (median) * .03):
         return 40 + score / median
     if (score > median - (median * .01) and score < median):
         return 45 + score / median
@@ -38,9 +37,10 @@ def categorizeScore(score, median):
         return 85 + score / median
     else:
         return 90 + score / median
-    
+
+
 def categorizeMedian(median):
-    if(median < 50):
+    if (median < 50):
         return median
     if (median > 50 and median < 85):
         return 45
@@ -48,17 +48,19 @@ def categorizeMedian(median):
         return 65
     if (median > 150 and median < 200):
         return 85
-    if(median > 200):
+    if (median > 200):
         return 92
+
 
 def getMean(JSON):
     counter = 0
     totalScore = 0
     for item in JSON["articles"]:
-        if(item["site_type"] == "claim"):
-            counter = counter  +1
+        if (item["site_type"] == "claim"):
+            counter = counter + 1
             totalScore = totalScore + int(item["score"])
     return totalScore / counter
+
 
 def printScores(JSON):
     mean = getMean(JSON)
@@ -68,11 +70,12 @@ def printScores(JSON):
         print(categorizeScore(item["score"], mean))
         print()
 
-def getHeadlineScore(title):
 
+def getHeadlineScore(title):
     title = title.replace(" ", "+")
     title = title.replace("_", "+")
     title = title.replace("-", "+")
+    title = title.replace(u"\u2019", "-")
 
     conn.request("GET", "/articles?sort_by=relevant&use_lucene_syntax=false&query=" + title, headers=hoaxiHeaders)
 
@@ -81,9 +84,9 @@ def getHeadlineScore(title):
     JSON = json.loads(data.decode("utf-8"))
     mean = getMean(JSON)
     return categorizeMedian(mean)
-    
 
-#Finds fact checking for the article or similar articles
+
+# Finds fact checking for the article or similar articles
 def createCheckUpdate(headline, URL):
     checkUrl = "https://adverifai-api.p.rapidapi.com/fact_check"
     checkQuerystring = {"headline": headline}
@@ -92,68 +95,73 @@ def createCheckUpdate(headline, URL):
     checkJSON = json.loads(checkData)
     printCheckInformation(checkJSON, URL, headline)
 
+
 def printCheckInformation(checkJSON, URL, headline):
     maxScore = []
+    description = createDescriptUpdate(URL)
+    print(description)
     for item in (checkJSON["fakeRef"]):
-        itemScore = float(getRoundedScore(item["score"])) + float(len(createDescriptUpdate(headline, URL)[0]) * 10)
+        if "fakeDescription" in description:
+            itemScore = float(getRoundedScore(item["score"])) + float(len(description["fakeDescription"]) * 10)
+        else:
+            itemScore = float(getRoundedScore(item["score"]))
         maxScore.append(itemScore)
     return max(maxScore)
-        
-    
-#Finds the score given by adverifi for the given claim
+
+
+# Finds the score given by adverifi for the given claim
 def createScoreUpdate(headline, URL):
     scoreUrl = "https://adverifai-api.p.rapidapi.com/fake_ref"
-    scoreQuerystring = {"headline":headline}
+    scoreQuerystring = {"headline": headline}
     scoreResponse = requests.request("GET", scoreUrl, headers=verifyHeaders, params=scoreQuerystring)
     scoreData = scoreResponse.text
     scoreJSON = json.loads(scoreData)
     return printCheckInformation(scoreJSON, URL, headline)
-    
 
-#Finds the descriptions for the domain to check if it is a source of any suspision
-def createDescriptUpdate(headline, URL):
+
+# Finds the descriptions for the domain to check if it is a source of any suspision
+def createDescriptUpdate(URL):
+    print(URL)
     descUrl = "https://adverifai-api.p.rapidapi.com/source_check"
-    descQuerystring = {"url":URL}
+    descQuerystring = {"url": URL}
     descResponse = requests.request("GET", descUrl, headers=verifyHeaders, params=descQuerystring)
     descData = descResponse.text
+    print(descData)
     descJSON = json.loads(descData)
-    return list(descJSON)
-    
+    print(descJSON)
+    return dict(descJSON)
+
+
 def printDescription(descJSON):
     descList = list(descJSON)
     return descList
-    
+
+
 def getDomain(URL):
     parsed_uri = urlparse(URL)
     result = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
     return result
+
 
 def getRoundedScore(score):
     score = int(score)
     score = score / 10
     result = str(round(score, 2))
     return result
-    
+
+
 def getVerifyScore(headline, URL):
     score = createScoreUpdate(headline, URL)
-    if ( score > 100 ):
+    if (score > 100):
         score = 99.9
     return score
-    
-def main(URL):
 
+
+def getScore(URL):
     headline = getHeadline(URL)
     verifyScore = getVerifyScore(headline, URL)
     hoaxyScore = getHeadlineScore(headline)
     print(verifyScore)
     print(hoaxyScore)
 
-    return (hoaxyScore + verifyScore) / 2
-    
-URL = "https://www.infowars.com/pizzagate-the-bigger-picture/"
-
-    
-print(main(URL))
-
-
-
+    return verifyScore
---
+++ PythonScripts/bot.py
@@ -0,0 +1,87 @@
+import tweepy
+import json
+import re
+import random
+import time
+from io import StringIO
+import csv
+import sqlite3
+from sqlite3 import Error
+from Hoaxy import getScore
+from newspaper import Article
+
+# Authenticate to Twitter and create API object
+auth = tweepy.OAuthHandler("xqslgSygms4vFIJEkFSziOGqf", "asDdYRmB5hPrwF3sQx9Bwkh8ZGHqZ61qHftOPHSLWgOSF387uL")
+auth.set_access_token("709929871892328448-gXGg7xlTKqLygDulQcRU4STkpEIz1gQ", "EokEp7r0Yxh3ZVV9W5m8O4FxlADucJlRphQcEn9dVUE6r")
+api = tweepy.API(auth)
+
+#Initalize database connection
+con = sqlite3.connect('twitter.db')
+cursor = con.cursor()
+
+# Makes sql database connection
+def connectToSQLDatabase():
+    try:
+        con = sqlite3.connect("twitter.db")
+        print("Database Connection established")
+    except Error:
+        print("error")
+
+def createTables():
+    # Tweet table
+    cursor.execute("CREATE TABLE tweets(tweet_id INTEGER PRIMARY KEY);")
+    cursor.execute("CREATE TABLE stats(articleTitle VARCHAR(50), articleURL VARCHAR(75), tweetCount INTEGER, "
+                   "PRIMARY KEY(articleTitle, articleURL))")
+    con.commit()
+
+def dropTables():
+    cursor.execute("DROP TABLE tweets")
+    cursor.execute("DROP TABLE stats")
+    con.commit()
+
+def insertID(tweet_id):
+    cursor.execute("INSERT INTO tweets (tweet_id) VALUES (?)", [tweet_id])
+    con.commit()
+
+def idExists(tweet_iD):
+    cursor.execute("SELECT * FROM tweets")
+    for x in cursor:
+        if tweet_iD == x[0]:
+            return True
+    return False
+
+def getHeadlineFromUrl(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.title
+
+def runBot():
+    while True:
+        mentions = api.mentions_timeline()
+        mention_list = mentions[0]
+        tweet_id = mention_list._json["id"]
+        expanded = mention_list._json["entities"]["urls"][0]["expanded_url"]
+        json_str = json.dumps(mention_list._json)
+        url = re.search("(?P<url>https?://[^\s]+)", json_str).group("url")
+        url = url[:len(url) - 2]
+
+        # Checks if the tweet_id has been seen already
+        if not idExists(tweet_id):
+            insertID(tweet_id)
+
+            scoring = getScore(expanded)
+            api.update_status('Article has a ' + str(scoring) + '% chance of being false ' + expanded)
+
+            print(tweet_id)
+            print(expanded)
+        else:
+            print("ID %d already seen", tweet_id)
+
+        time.sleep(15)
+
+def main():
+    runBot()
+
+if __name__ == "__main__":
+    main()
